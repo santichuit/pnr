0b3712a99ceb7dfa86d52e788a68dd201fe905a3
Cloud User <centos@ip-172-31-25-9.eu-central-1.compute.internal>
2025-02-19 13:42:24 +0000

    fix code

--- payoneer/src/app/app.component.spec.ts
+++ payoneer/src/app/app.component.spec.ts
@@ -20,10 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'payoneer'`, () => {
+  it(`should have as title 'Payoneer'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('payoneer');
+    expect(app.title).toEqual('Payoneer');
   });
 
   it('should render title', () => {
